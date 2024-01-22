484f48e54c8c87b46683d944b69f83dcd2c1a979
Hacktiv8or <152891466+Hacktiv8or@users.noreply.github.com>
2024-01-22 16:44:14 +0530

    Committing files

--- uploads/asdf.py
+++ uploads/asdf.py
@@ -1,12 +1,53 @@
-n = int(input("Enter the number of rows: "))
-s = input("symbol to use to generate pattern: ")
-printed = None
-print(f"\n\n {'-'*(round(n/2)+5)} Your Pattern {'-'*(round(n/2)+5)} \n\n")
-for x in range(0, n*2, 2):
-    if x >= n: times = n-(x-n+1)
-    else: times = x+1
-    string = " ".join([x for x in s*times])
-    space_needed = " " * round((n*2-len(string))/2)
-    final_string = space_needed + string
-    if final_string != printed: print(final_string)
-    printed = final_string
+import os
+import time
+import random
+
+os.system('color 0a')
+
+nums = [1,0]
+tm = 0
+
+while tm < 30:
+    print(random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),)
+
+    tm = tm + 0.1
+
+    time.sleep(0.1)
