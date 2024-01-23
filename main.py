d30462d0cc8c37b8331b1acb4c8889a7bf814b21
Hacktiv8or <152891466+Hacktiv8or@users.noreply.github.com>
2024-01-23 19:59:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ repo_name = 'Storage'
 user = g.get_user(username)
 repo = user.get_repo(repo_name)
 
-st.title("~Hackshpere~")
+st.title("Hackshpere~")
 upload = st.file_uploader("Choose a file")
 # if upload is not None:
 #     # Read the content of the BytesIO object
