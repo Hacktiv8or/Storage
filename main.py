4b9ed6c8576792a39524054c341b916c6102f607
Hacktiv8or <152891466+Hacktiv8or@users.noreply.github.com>
2024-01-26 21:43:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if tab == "Download":
         f'{file.name} - '
         f'<a style="display:inline-block;padding:6px 12px;margin-bottom:0;font-size:14px;font-weight:400;line-height:1.42857143;text-align:center;white-space:nowrap;vertical-align:middle;cursor:pointer;border:1px solid transparent;border-radius:4px;background-color:#337ab7;color:#fff;border-color:#2e6da4;text-decoration:none;" '
         f'href="{file.download_url}" download="{file.name}">'
-        f'<span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Download'
+        f'<span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Downloadd'
         f'</a>',
         unsafe_allow_html=True
     )
