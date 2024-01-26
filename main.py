23d4cedd66429c8baa6675352243a97627841c36
Hacktiv8or <152891466+Hacktiv8or@users.noreply.github.com>
2024-01-26 21:37:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -92,11 +92,17 @@ if tab == "Download":
     # st.write(f"File Size: {round(file.size/1000000, 2)} MB")
     st.write(f"File Name: {file.name}")
     st.write(f"File Size: {round(file.size/1000000,2)} MB ")
-    
-    def download():
-        st.markdown(f'<a href="{file.download_url}" download="{file.name}"></a>', unsafe_allow_html=True)
-    # Create a download button next to each file name
-    download_button = st.button(f"Download⬇️", key=file.name, on_click=download())
+    st.markdown(
+        f'<a style="display:inline-block;padding:6px 12px;margin-bottom:0;font-size:14px;font-weight:400;line-height:1.42857143;text-align:center;white-space:nowrap;vertical-align:middle;cursor:pointer;border:1px solid transparent;border-radius:4px;background-color:#337ab7;color:#fff;border-color:#2e6da4;text-decoration:none;" '
+        f'href="{file.download_url}" download="{file.name}">'
+        f'<span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Download {file.name}'
+        f'</a>',
+        unsafe_allow_html=True
+    )
+    # def download():
+    #     st.markdown(f'<a href="{file.download_url}" download="{file.name}"></a>', unsafe_allow_html=True)
+    # # Create a download button next to each file name
+    # download_button = st.button(f"Download⬇️", key=file.name, on_click=download())
 
     # If the download button is clicked, initiate the download
 
